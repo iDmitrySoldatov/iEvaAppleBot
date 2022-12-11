57623912ad5158e8eb59102e6562c81f277a3ec0
Dmitry Soldatov <dmitriysoldatov1998@gmail.com>
2022-12-11 14:03:40 +0500

    Create Project

---
+++ src/main/java/ru/dmitrysoldatov/evaapple/bot/iEvaAppleBot/IEvaAppleBotApplication.java
@@ -0,0 +1,13 @@
+package ru.dmitrysoldatov.evaapple.bot.iEvaAppleBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IEvaAppleBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IEvaAppleBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dmitrysoldatov/evaapple/bot/iEvaAppleBot/IEvaAppleBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dmitrysoldatov.evaapple.bot.iEvaAppleBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IEvaAppleBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
